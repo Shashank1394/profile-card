4b65762d93dd8be084df4350ffd6b30b854c9810
Shashank Jadhav <shashankjadhav13940@gmail.com>
2025-02-17 20:23:08 +0530

    enhance Header component with improved layout and styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,13 @@
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="relative h-screen w-full">
+      <div className="absolute inset-0 flex items-center justify-center">
+        <span className="absolute z-0 mr-[270px] mb-[90px] text-5xl font-bold">
+          The
+        </span>
+        <span className="absolute z-10 text-9xl">Company</span>
+      </div>
+    </div>
+  );
 }
 export default Header;
